3cb2e265213699ae10142835e147e5ce059d423a
Dyvinia <dulana57@gmail.com>
2024-09-05 18:33:58 -0400

    version

--- blender_qfix.py
+++ blender_qfix.py
@@ -2,7 +2,7 @@ bl_info = {
     "name": "Blender QFix",
     "author": "Dyvinia",
     "description": "Fixes interpolation issues when importing animations from games that use SLERPed Quaternions for rotations.",
-    "version": (1, 0, 4),
+    "version": (1, 0, 5),
     "blender": (4, 0, 0),
     "category": "Animation",
 }
