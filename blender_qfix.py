3ab46d46b6382eb35043a4b74de3d97c36ceb868
Dyvinia <dulana57@gmail.com>
2024-09-05 18:33:47 -0400

    should work with nla tracks now

--- blender_qfix.py
+++ blender_qfix.py
@@ -24,63 +24,80 @@ class ANIM_OT_QFix(Operator):
     def execute(self, context):
         obj = context.object
         bone_names = [b.name for b in obj.pose.bones]
-        fcurves = obj.animation_data.action.fcurves
+        
+        if obj.animation_data:
+            if obj.animation_data.action:
+                fcurves = obj.animation_data.action.fcurves
 
-        wm = bpy.context.window_manager
-        wm.progress_begin(0, len(fcurves))
+                wm = bpy.context.window_manager
+                wm.progress_begin(0, len(fcurves))
 
-        for curve_index in range(len(fcurves)):
-            curve = fcurves[curve_index]
-            
-            # only bone keyframes
-            if '.' not in curve.data_path:
-                continue
+                fix_interpolation(fcurves, bone_names, wm)
 
-            # iterate through all the bones' fcurves' w channel
-            if curve.data_path.split('"')[1] in bone_names and curve.array_index == 0 and curve.data_path.split('.')[-1] == 'rotation_quaternion':
-                # w will be current index's fcurve, x is next fcurve, etc
-                fqw = fcurves[curve_index]
-                fqx = fcurves[curve_index + 1]
-                fqy = fcurves[curve_index + 2]
-                fqz = fcurves[curve_index + 3]
+                wm.progress_end()
 
-                # invert quaternion so that interpolation takes the shortest path
-                endQuat = 0
-                if (len(fqw.keyframe_points) > 0):
-                    endQuat = Quaternion((fqw.keyframe_points[0].co[1],
-                                          fqx.keyframe_points[0].co[1],
-                                          fqy.keyframe_points[0].co[1],
-                                          fqz.keyframe_points[0].co[1]))
+            for track in obj.animation_data.nla_tracks:
+                for strip in track.strips:
+                    fcurves = strip.action.fcurves
                     
-                    fqw.keyframe_points[0].interpolation = "LINEAR"
-                    fqx.keyframe_points[0].interpolation = "LINEAR"
-                    fqy.keyframe_points[0].interpolation = "LINEAR"
-                    fqz.keyframe_points[0].interpolation = "LINEAR"
+                    wm = bpy.context.window_manager
+                    wm.progress_begin(0, len(fcurves))
 
-                    for i in range(len(fqw.keyframe_points)):
-                        startQuat = endQuat
-                        endQuat = Quaternion((fqw.keyframe_points[i].co[1],
-                                              fqx.keyframe_points[i].co[1],
-                                              fqy.keyframe_points[i].co[1],
-                                              fqz.keyframe_points[i].co[1]))
-    
-                        if startQuat.dot(endQuat) < 0:
-                            endQuat.negate()
-                            fqw.keyframe_points[i].co[1] = -fqw.keyframe_points[i].co[1]
-                            fqx.keyframe_points[i].co[1] = -fqx.keyframe_points[i].co[1]
-                            fqy.keyframe_points[i].co[1] = -fqy.keyframe_points[i].co[1]
-                            fqz.keyframe_points[i].co[1] = -fqz.keyframe_points[i].co[1]
-                        
-                        fqw.keyframe_points[i].interpolation = "LINEAR"
-                        fqx.keyframe_points[i].interpolation = "LINEAR"
-                        fqy.keyframe_points[i].interpolation = "LINEAR"
-                        fqz.keyframe_points[i].interpolation = "LINEAR"
-                        
-            wm.progress_update(curve_index)
+                    fix_interpolation(fcurves, bone_names, wm)
 
-        wm.progress_end()
+                    wm.progress_end()
         
         return {'FINISHED'}
+    
+def fix_interpolation(fcurves, bone_names, wm):
+    for curve_index in range(len(fcurves)):
+        curve = fcurves[curve_index]
+            
+        # only bone keyframes
+        if '.' not in curve.data_path:
+            continue
+
+        # iterate through all the bones' fcurves' w channel
+        if curve.data_path.split('"')[1] in bone_names and curve.array_index == 0 and curve.data_path.split('.')[-1] == 'rotation_quaternion':
+            # w will be current index's fcurve, x is next fcurve, etc
+            fqw = fcurves[curve_index]
+            fqx = fcurves[curve_index + 1]
+            fqy = fcurves[curve_index + 2]
+            fqz = fcurves[curve_index + 3]
+
+            # invert quaternion so that interpolation takes the shortest path
+            endQuat = 0
+            if (len(fqw.keyframe_points) > 0):
+                endQuat = Quaternion((fqw.keyframe_points[0].co[1],
+                                      fqx.keyframe_points[0].co[1],
+                                      fqy.keyframe_points[0].co[1],
+                                      fqz.keyframe_points[0].co[1]))
+                
+                fqw.keyframe_points[0].interpolation = "LINEAR"
+                fqx.keyframe_points[0].interpolation = "LINEAR"
+                fqy.keyframe_points[0].interpolation = "LINEAR"
+                fqz.keyframe_points[0].interpolation = "LINEAR"
+
+                for i in range(len(fqw.keyframe_points)):
+                    startQuat = endQuat
+                    endQuat = Quaternion((fqw.keyframe_points[i].co[1],
+                                          fqx.keyframe_points[i].co[1],
+                                          fqy.keyframe_points[i].co[1],
+                                          fqz.keyframe_points[i].co[1]))
+    
+                    if startQuat.dot(endQuat) < 0:
+                        endQuat.negate()
+                        fqw.keyframe_points[i].co[1] = -fqw.keyframe_points[i].co[1]
+                        fqx.keyframe_points[i].co[1] = -fqx.keyframe_points[i].co[1]
+                        fqy.keyframe_points[i].co[1] = -fqy.keyframe_points[i].co[1]
+                        fqz.keyframe_points[i].co[1] = -fqz.keyframe_points[i].co[1]
+                    
+                    fqw.keyframe_points[i].interpolation = "LINEAR"
+                    fqx.keyframe_points[i].interpolation = "LINEAR"
+                    fqy.keyframe_points[i].interpolation = "LINEAR"
+                    fqz.keyframe_points[i].interpolation = "LINEAR"
+                    
+        wm.progress_update(curve_index)
 
 class PANEL_PT_QFix(Panel):
     bl_label = "QFix"
